5ce8bb244c1443c9f1443b02b5888a3328960608
enocklubowa <lubowaenock34@gmail.com>
2020-11-23 21:24:27 +0300

    --added chat and phone buttons on product listing screen

--- app/src/main/java/com/example/demo/ViewHolder/ProductAdapter.java
+++ app/src/main/java/com/example/demo/ViewHolder/ProductAdapter.java
@@ -1,33 +1,57 @@
 package com.example.demo.ViewHolder;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.elyeproj.loaderviewlibrary.LoaderImageView;
+import com.example.demo.ChatActivity;
 import com.example.demo.ProductDetailsActivity;
 import com.example.demo.R;
+import com.example.demo.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
 public class ProductAdapter extends RecyclerView.ViewHolder{
     public View view;
     private TextView name_field, price_field, location_field;
+    private Button callButton, chatButton;
+    private String sellerPhone;
+    private static final int REQUEST_PHONE_CALL = 1;
+
 
     public ProductAdapter(@NonNull View itemView) {
         super(itemView);
         view = itemView;
     }
 
-    public void setDetails(String name, String location , String price, final String imageUrl, final String userId, final String description){
+    public void setDetails(String name, String location , String price, final String imageUrl, final String sellerId, final String description){
+        getSellerPhone(sellerId);
         name_field = (TextView) view.findViewById(R.id.product_name);
         price_field = (TextView) view.findViewById(R.id.product_price);
         location_field = (TextView) view.findViewById(R.id.product_location);
+        callButton = (Button) view.findViewById(R.id.call_button);
+        chatButton = (Button) view.findViewById(R.id.chat_button);
         LoaderImageView image = (LoaderImageView) view.findViewById(R.id.product_cover_image);
         LinearLayout clickable_view = (LinearLayout) view.findViewById(R.id.lyt_parent);
 
@@ -35,7 +59,35 @@ public class ProductAdapter extends RecyclerView.ViewHolder{
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        goToProductDetailsActivity(userId, imageUrl, description);
+                        goToProductDetailsActivity(sellerId, imageUrl, description);
+                    }
+                }
+        );
+
+        callButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(ContextCompat.checkSelfPermission(view.getContext(), Manifest.permission.CALL_PHONE)
+                                != PackageManager.PERMISSION_GRANTED){
+                            ActivityCompat.requestPermissions((Activity) view.getContext(), new String[]{Manifest.permission.CALL_PHONE}
+                            ,REQUEST_PHONE_CALL);
+
+                        }
+                        else {
+                            Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + sellerPhone));
+                            view.getContext().startActivity(intent);
+                        }
+
+                    }
+                }
+        );
+
+        chatButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openChatActivity(sellerId);
                     }
                 }
         );
@@ -64,6 +116,38 @@ public class ProductAdapter extends RecyclerView.ViewHolder{
         extras.putString("LOCATION", location_field.getText().toString());
         extras.putString("IMAGE_URL", imageUrl);
         extras.putString("DESCRIPTION", description);
+        intent.putExtras(extras);
+        view.getContext().startActivity(intent);
+    }
+
+    private void getSellerPhone(String sellerId){
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("users").child(sellerId);
+        ref.addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        if(user != null){
+                            setUserPhone(user.getPhone());
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                }
+        );
+    }
+
+    private void setUserPhone(String phone) {
+        sellerPhone = phone;
+    }
+
+    public void openChatActivity(String sellerId){
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle extras = new Bundle();
+        extras.putString("SELLER_ID", sellerId);
         intent.putExtras(extras);
         view.getContext().startActivity(intent);
     }
